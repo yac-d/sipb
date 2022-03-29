10cb4a5723e47ca20a1563f05b007bdf02302b06
Prithvi Vishak <prithvivishak@gmail.com>
2022-03-29 16:47:15 +0530

    Make file list refresh only after an upload is complete

--- webpages/pastebin/uploader.js
+++ webpages/pastebin/uploader.js
@@ -1,12 +1,28 @@
-function uploadFile(form) {
-	chooser = document.getElementById("filechooser");
-	if (chooser.value == "") {
-		alert("Select a file first!");
-		return;
-	}
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "/upload", true);
-	xhr.send(new FormData(uploadForm));
-	chooser.value = "";
-	loadFiles();
+function uploadFile() {
+	return new Promise(function(resolve, reject) {
+		chooser = document.getElementById("filechooser");
+		if (chooser.value == "") {
+			reject(new Error("File to upload not selected"));
+			return;
+		}
+
+		let xhr = new XMLHttpRequest
+		xhr.onreadystatechange = function() {
+			if (this.readyState == 4) {
+				console.log("Request complete.");
+				resolve("Done!");
+			}
+		}
+		xhr.open("POST", "/upload", true);
+		xhr.send(new FormData(uploadForm));
+	});
+}
+
+function uploadAndRefresh() {
+	let p = uploadFile();
+	p.then(function() {
+		chooser.value = "";
+		loadFiles();
+	});
+	p.catch((e) => alert("Select a file first!"));
 }
