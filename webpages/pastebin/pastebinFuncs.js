05c6d55769dbae654cfcc271dcfcb6b17c4971e5
Prithvi Vishak <prithvivishak@gmail.com>
2022-04-02 16:13:43 +0530

    Add timestamp to pastebin webpage

--- webpages/pastebin/pastebinFuncs.js
+++ webpages/pastebin/pastebinFuncs.js
@@ -26,10 +26,13 @@ function populateFileContainer(details, fileContainer) {
 	let pathElements = details.Path.split("/");
 	let name = pathElements[pathElements.length - 1];
 	let prettyName = name.substr(name.search("_")+1, name.length);
+	let date = new Date(parseInt(name.substr(0, name.search("_"))));
 
 	console.log(fileContainer);
 	let filename = document.createElement("h3");
 	let link = document.createElement("a");
+	let timestamp = document.createElement("p");
+	timestamp.innerText = "Uploaded " + date.toLocaleString();
 	link.href = details.Path;
 	filename.innerText = prettyName;
 	link.append(filename);
@@ -44,6 +47,8 @@ function populateFileContainer(details, fileContainer) {
 
 		fileContainer.append(img);
 	}
+
+	fileContainer.append(timestamp);
 }
 
 function setFileCnt(cnt) {
