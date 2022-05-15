e9d8c60dedd5e56923816bcba5bb652ab1192e6b
Prithvi Vishak <prithvivishak@gmail.com>
2022-05-15 11:20:03 +0530

    Create logger to handle all logging

--- server/main.go
+++ server/main.go
@@ -1,65 +1,32 @@
 package main
 
 import (
-	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/Eeshaan-rando/sipb/src/configdef"
-	"github.com/Eeshaan-rando/sipb/src/filebin"
 	"github.com/Eeshaan-rando/sipb/src/filebin/simplefb"
 	"github.com/Eeshaan-rando/sipb/src/httpsrv"
+	"github.com/Eeshaan-rando/sipb/src/logger"
 )
-
-func logFileSave(result filebin.SaveFileResult) {
-	log.Printf("Saving file %s", result.Filename)
-	if result.Error != nil {
-		log.Printf("Error while saving file: %s", result.Error)
-	}
-	if result.TruncatedBytes > 0 {
-		log.Printf("File %s above set size limit, truncating by %d bytes", result.Filename, result.TruncatedBytes)
-	}
-}
-
-func logFileDetailsRequest(result filebin.FileDetailsResult) {
-	log.Printf("Details for file %s requested", result.Filename)
-	if result.Error != nil {
-		log.Printf("Error while getting file details: %s")
-	}
-}
-
-func logFileCountRequest(result filebin.FileCountResult) {
-	if result.Error != nil {
-		log.Printf("Error while getting file count: %s", result.Error)
-	} else {
-		log.Printf("File count requested; currently at %d", result.Count)
-	}
-}
 
 func main() {
 	var config configdef.Config
-	if err := config.ReadFromYAML("./config.yaml"); err != nil {
-		log.Fatalln("Error reading configuration from ./config.yaml")
-	}
-	log.Printf("Read configuration from ./config.yaml")
-
+	logger.LogConfigRead("./config.yaml", config.ReadFromYAML("./config.yaml"))
 	// Overrides config from file only for environment variables that are set (unset ones are ignored)
-	if err := config.ReadFromEnvVars(); err != nil {
-		log.Fatalln("Error reading configuration from environment variables")
-	}
-	log.Printf("Read configuration from environment variables")
+	logger.LogConfigRead("environment variables", config.ReadFromEnvVars())
 
 	var bin = simplefb.New(config)
 	var srv = httpsrv.New(config, bin)
-	srv.OnSave = logFileSave
-	srv.OnDetailsRequested = logFileDetailsRequest
-	srv.OnCountRequested = logFileCountRequest
+	srv.OnSave = logger.LogFileSave
+	srv.OnDetailsRequested = logger.LogFileDetailsRequest
+	srv.OnCountRequested = logger.LogFileCountRequest
 
-	log.Println(srv.Start())
+	logger.Log(srv.Start())
 
 	var terminator = make(chan os.Signal, 1)
 	signal.Notify(terminator, os.Interrupt, syscall.SIGTERM)
 	<-terminator
-	log.Println("Exiting")
+	logger.Log("Exiting")
 }
---
+++ server/src/logger/logger.go
@@ -0,0 +1,44 @@
+package logger
+
+import (
+	"log"
+
+	"github.com/Eeshaan-rando/sipb/src/filebin"
+)
+
+func LogConfigRead(source string, err error) {
+	if err != nil {
+		log.Println("Error reading configuration from", source)
+		log.Fatalln(err)
+	}
+	log.Println("Read configuration from", source)
+}
+
+func LogFileSave(result filebin.SaveFileResult) {
+	log.Printf("Saving file %s", result.Filename)
+	if result.Error != nil {
+		log.Printf("Error while saving file: %s", result.Error)
+	}
+	if result.TruncatedBytes > 0 {
+		log.Printf("File %s above set size limit, truncating by %d bytes", result.Filename, result.TruncatedBytes)
+	}
+}
+
+func LogFileDetailsRequest(result filebin.FileDetailsResult) {
+	log.Printf("Details for file %s requested", result.Filename)
+	if result.Error != nil {
+		log.Printf("Error while getting file details: %s")
+	}
+}
+
+func LogFileCountRequest(result filebin.FileCountResult) {
+	if result.Error != nil {
+		log.Printf("Error while getting file count: %s", result.Error)
+	} else {
+		log.Printf("File count requested; currently at %d", result.Count)
+	}
+}
+
+func Log(thing any) {
+	log.Println(thing)
+}
